e8aa04d1713f1c807ed5b5939cb43859badfd83d
eshaan deshmukh <eshaan2031@icloud.com>
2025-11-07 22:05:34 +0000

    Some basic data structures

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum token_type;
+char** symbols;
+typedef struct token_struct token;
+
+enum node_type;
+typedef struct node_struct node;
+
+int main(){
+	char* symbols[] = {"=", "+=", "-="};
+
+	enum token_type {
+		WORD = 128 + sizeof(symbols)/sizeof(symbols[0]), // 128 because that's where ASCII ends
+		INT,
+		FLOAT
+	};
+
+	enum node_type {
+		PROGRAM = 128 + sizeof(symbols)/sizeof(symbols[0]),
+	};
+
+	struct token_struct {
+		enum token_type type;
+		char* argument;
+		token* carry;
+	};
+
+}
