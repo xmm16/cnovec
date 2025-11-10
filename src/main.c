1936c87bf726f50913c7b6750345935b41afb2ae
eshnd <eshaan2031@icloud.com>
2025-11-10 17:52:39 -0600

    Organized the code a little bit

--- src/main.c
+++ src/main.c
@@ -3,11 +3,52 @@
 #include <string.h>
 
 enum token_type;
-char** symbols;
 struct token_struct;
+char* symbols[] = {"+=", "-="};
 
 enum node_type;
 struct node_struct;
+
+enum token_type {
+  WORD = 128 + sizeof(symbols)/sizeof(symbols[0]), // 128 because that's where ASCII ends
+  INT,
+  FLOAT
+};
+
+enum node_type {
+  PROGRAM = 128 + sizeof(symbols)/sizeof(symbols[0]),
+};
+
+typedef struct token_struct {
+  enum token_type type;
+  char* string_argument;
+  struct token_struct* token_argument;
+} token;
+
+typedef struct node_struct {
+  enum node_type type;
+  token* token_argument;
+  struct node_struct* back;
+  struct node_struct* left;
+  struct node_struct* right;
+} node;
+
+void append_token(token** code_lex, size_t* code_lex_size_ptr, size_t* code_lex_index_ptr, enum token_type type, char* string_argument, token* token_argument){ 
+  size_t code_lex_size = *code_lex_size_ptr;
+  size_t code_lex_index = *code_lex_index_ptr;
+
+  while (code_lex_size < (code_lex_index + 1) * sizeof(token)){
+    code_lex_size *= 2;
+    *code_lex = realloc(*code_lex, code_lex_size);
+  }
+
+  code_lex[code_lex_index]->type = type;
+  code_lex[code_lex_index]->string_argument = string_argument;
+  code_lex[code_lex_index]->token_argument = token_argument;
+
+  
+  code_lex_index++;
+}
 
 int check_different_sized_strings(char* original_string_literal, char* check_string_literal){
     size_t strlen_check_string = strlen(check_string_literal);
@@ -44,32 +85,6 @@ int check_different_sized_strings(char* original_string_literal, char* check_str
 }
 
 int main(int argc, char** argv){
-	char* symbols[] = {"+=", "-="};
-
-	enum token_type {
-		WORD = 128 + sizeof(symbols)/sizeof(symbols[0]), // 128 because that's where ASCII ends
-		INT,
-		FLOAT
-	};
-
-	enum node_type {
-		PROGRAM = 128 + sizeof(symbols)/sizeof(symbols[0]),
-	};
-
-	typedef struct token_struct {
-		enum token_type type;
-		char* string_argument;
-		struct token_struct* token_argument;
-	} token;
-
-	typedef struct node_struct {
-		enum node_type type;
-		token* token_argument;
-		struct node_struct* back;
-		struct node_struct* left;
-		struct node_struct* right;
-	} node;
-
 	size_t strlen_argv_1 = strlen(argv[1]); // "argv[1]" because I don't want to have to deal with file management until I need to
 	char* raw_code = argv[1];
 
@@ -87,17 +102,8 @@ int main(int argc, char** argv){
 			int buf_size = i - quote_buf_start;
 			char* quote_arg = malloc(1 + buf_size); // free every string argument in quotes
 			strncpy(quote_arg, &raw_code[quote_buf_start], buf_size);
-			
-			while (code_lex_size < (code_lex_index + 1) * sizeof(token)){
-				code_lex_size *= 2;
-				code_lex = realloc(code_lex, code_lex_size);
-			}
+      append_token(&code_lex, &code_lex_size, &code_lex_index, '"', quote_arg, NULL);
 
-			code_lex[code_lex_index].type = '"';
-			code_lex[code_lex_index].string_argument = quote_arg;
-			code_lex[code_lex_index].token_argument = NULL;
-			
-			code_lex_index++;
 			continue;
 		} else if (quote_mode) continue;
 
@@ -108,6 +114,8 @@ int main(int argc, char** argv){
 			continue;
 		}
 	}
+
+  printf("%s\n", code_lex[0].string_argument);
 
 	node code_tree;
 	code_tree.type = PROGRAM;
